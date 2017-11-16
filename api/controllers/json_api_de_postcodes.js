b42c16431435c868bc9c003bf46fa31d7740a78d
arne-at-daten-und-bass <arne@daten-und-bass.io>
2017-11-16 11:43:33 +0100

    Changed to postcodes.js and complete rewrite for Logging and DB import

--- api/controllers/json_api_de_postcodes.js
+++ api/controllers/json_api_de_postcodes.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var request = require('request');
+var postcodes = require('./postcodes');
 
 var jsonApiDePostcodes = function (api) {
 
@@ -8,101 +9,111 @@ var jsonApiDePostcodes = function (api) {
 
   var pcNamesZKey = api.parameters.de.name + ":" + api.parameters.postcodes.name + ":names";
   var pcPositionsZKey = api.parameters.de.name + ":" + api.parameters.postcodes.name + ":positions";
-  var pcObjectsHPrefix = api.parameters.de.name + ":" + api.parameters.postcodes.name+ ":object";
+  var pcObjectsHPrefix = api.parameters.de.name + ":" + api.parameters.postcodes.name + ":object";
 
   var pcNamesZScore = 0;
 
   return {
     load: function (req, res, next) {
-      // [{"osm_city_id":"2803631","city":"Allmendingen","postcode":"89604","district":"Alb-Donau-Kreis","state":"Baden-Wuerttemberg"},
-      //  {"osm_city_id":"62422","city":"Berlin","postcode":"10247","district":"","state":"Berlin"}, ...]
-      var pc = [];
-      var pcReqIsSuccess = false;
-      var pcReqNoSuccess = [];
 
-      that.logger.counter.error = 0;
-      that.logger.counter.warn = 0;
-      that.logger.counter.info = 0;
+      that.logger.reader('info-file', ['counters', 'timestamp'], 0, function (error, infoFileCounters) {
 
-      pc.forEach(function (postcode, index) {
+        console.log(infoFileCounters);
+        that.logger.counter.error = infoFileCounters.error || 0;
+        that.logger.counter.warn = infoFileCounters.warn || 0;
+        that.logger.counter.info = infoFileCounters.info || 0;
 
-        var pcReqUrl = process.env.DNB_ENV_APP_S1_WB_REQUEST_BASE_URL + postcode.state + '/' + postcode.postcode + process.env.DNB_ENV_APP_S1_WB_REQUEST_QUERY_PARAMS;
+        var indexInLogs = (infoFileCounters.error + infoFileCounters.warn + infoFileCounters.info) / 3;
+        var amountNext = 50;
+        var pc = postcodes.slice(indexInLogs, indexInLogs + amountNext)
+        console.log(indexInLogs);
+        console.log(indexInLogs + amountNext);
+        console.log(pc.length);
+        console.log(postcodes[indexInLogs])
+        console.log(pc[0])
+        console.log(postcodes[indexInLogs + amountNext - 1])
+        console.log(pc[49])
+        // console.log(pc);
 
-        request(pcReqUrl, function (error, response, body) {
+        pc.forEach(function (postcode, index) {
 
-          if (error) {
+          var pcReqIsSuccess = false;
+          var pcReqUrl = process.env.DNB_ENV_APP_S1_WB_REQUEST_BASE_URL + postcode.state + '/' + postcode.postcode + process.env.DNB_ENV_APP_S1_WB_REQUEST_QUERY_PARAMS;
+          
+          var pcNamesZMember = "";
+          var pcObjectsHKey  = "";
+          var pcObjectsHValueBBX = "";
+          
+          var logData = {};
 
-            var logData = that.logger.utils.dataMaker(error, 'pcReq', null, postcode.postcode + " " + postcode.city );
-            that.logger.writer(logData);
-            pcReqNoSuccess.push(pc[index]);
+          request(pcReqUrl, function (error, response, body) {
 
-          }
-          if (!error && response.statusCode == 200) {
+            if (error) {
 
-            JSON.parse(body).forEach(function (entry, bodyIndex) {
+              logData = that.logger.utils.dataMaker(error, 'pcReq', null, postcode, index);
+              that.logger.writer(error, logData);
+            }
 
-              if(entry.osm_type === "relation" && entry.type === "postcode") {
-                
-                pcReqIsSuccess = true;
-                
-                var pcNamesZMember = "";
-                var pcObjectsHValueBBX = entry.boundingbox.join();
+            if (!error && response.statusCode == 200) {
+              
+              if (JSON.parse(body).length === 0 ) {
+                logData = that.logger.utils.dataMaker(null, 'pcReq200Empty', response.statusCode, postcode, index);
+                that.logger.writer(null, logData); 
+              }
 
-                if(postcode.district === "") {
+              JSON.parse(body).forEach(function (entry, bodyIndex) {
 
-                  pcNamesZMember = postcode.postcode + " " + postcode.city + " " + entry.display_name.split(",")[0];
+                if(entry.osm_type === "relation" && entry.type === "postcode" && pcReqIsSuccess === false) {
+                  
+                  pcReqIsSuccess = true;
 
-                  that.db.names.add(pcNamesZKey, [pcNamesZScore, pcNamesZMember], that.logger.writer);
-                  that.db.positions.add(pcPositionsZKey, [entry.lon, entry.lat, postcode.postcode], that.logger.writer);
-
+                  pcObjectsHKey = pcObjectsHPrefix + ":" + postcode.postcode;
+                  pcObjectsHValueBBX = entry.boundingbox.join();
                   delete entry.boundingbox;
                   entry.boundingbox = pcObjectsHValueBBX;
-                  that.db.objects.add(pcObjectsHPrefix + ":" + postcode.postcode, entry, that.logger.writer);
 
-                } else {
+                  if(postcode.district === "") {
 
-                  pcNamesZMember = postcode.postcode + " " + entry.display_name.split(",")[0];
+                    pcNamesZMember = postcode.postcode + " " + postcode.city + " " + entry.display_name.split(",")[0];
                     
-                  if(entry.display_name.split(",")[0].indexOf(postcode.city) === -1 && entry.display_name.split(",")[1].indexOf(postcode.city) === -1) {
-                    
-                    pcNamesZMember = pcNamesZMember.concat(" ", postcode.city);
+                  } else {
+
+                    pcNamesZMember = postcode.postcode + " " + entry.display_name.split(",")[0];
+                      
+                    if(entry.display_name.split(",")[0].indexOf(postcode.city) === -1 && entry.display_name.split(",")[1].indexOf(postcode.city) === -1) {
+                      
+                      pcNamesZMember = pcNamesZMember.concat(" ", postcode.city);
+                    }
+
+                    if(entry.display_name.split(",")[1].indexOf("Landkreis") === -1 && entry.display_name.split(",")[1].indexOf("Verbandsgemeinde") === -1 &&
+                       entry.display_name.split(",")[1].indexOf("VGem") === -1 && entry.display_name.split(",")[1].indexOf("Kreis") === -1 &&
+                       entry.display_name.split(",")[1].indexOf("Amt") === -1 && entry.display_name.split(",")[1].indexOf("Samtgemeinde") === -1 &&
+                       entry.display_name.split(",")[1].indexOf("-Land") === -1 && entry.display_name.split(",")[1].indexOf("Verwaltungsverband") === -1 && 
+                       entry.display_name.split(",")[1].indexOf("Verwaltungsgemeinschaft") === -1 && entry.display_name.split(",")[1].indexOf("GVV") === -1 &&
+                       entry.display_name.split(",")[1].indexOf("Region") === -1 && entry.display_name.split(",")[1].indexOf("Gemeindeverwaltungsverband") === -1 &&
+                       pcNamesZMember.indexOf(entry.display_name.split(",")[1]) === -1) { 
+
+                       pcNamesZMember = pcNamesZMember.concat(entry.display_name.split(",")[1]);
+                    }
                   }
-                  // replaced umlauts in thueringen und wuerttemberg
-                  // added quotation marks for OSM_id and postcode
-                  // removed Städteregion Aachen in district where city is Aachen
-                  if(entry.display_name.split(",")[1].indexOf("Landkreis") === -1 && entry.display_name.split(",")[1].indexOf("Verbandsgemeinde") === -1 &&
-                     entry.display_name.split(",")[1].indexOf("VGem") === -1 && entry.display_name.split(",")[1].indexOf("Kreis") === -1 &&
-                     entry.display_name.split(",")[1].indexOf("Amt") === -1 && entry.display_name.split(",")[1].indexOf("Samtgemeinde") === -1 &&
-                     entry.display_name.split(",")[1].indexOf("-Land") === -1 && entry.display_name.split(",")[1].indexOf("Verwaltungsverband") === -1 && 
-                     entry.display_name.split(",")[1].indexOf("Verwaltungsgemeinschaft") === -1 && entry.display_name.split(",")[1].indexOf("GVV") === -1 &&
-                     entry.display_name.split(",")[1].indexOf("Region") === -1 && entry.display_name.split(",")[1].indexOf("Gemeindeverwaltungsverband") === -1 &&
-                     pcNamesZMember.indexOf(entry.display_name.split(",")[1]) === -1) { 
 
-                    pcNamesZMember = pcNamesZMember.concat(entry.display_name.split(",")[1]);
-                  }
-
-                  that.db.names.add(pcNamesZKey, [pcNamesZScore, pcNamesZMember], that.logger.writer);
-                  that.db.positions.add(pcPositionsZKey, [entry.lon, entry.lat, postcode.postcode], that.logger.writer);
-
-                  delete entry.boundingbox;
-                  entry.boundingbox = pcObjectsHValueBBX;
-                  that.db.objects.add(pcObjectsHPrefix + ":" + postcode.postcode, entry, that.logger.writer);
-
+                  that.db.names.add(pcNamesZKey, [pcNamesZScore, pcNamesZMember], {postcode, index}, that.logger.writer);
+                  that.db.positions.add(pcPositionsZKey, [entry.lon, entry.lat, postcode.postcode], {postcode, index}, that.logger.writer);
+                  that.db.objects.add(pcObjectsHKey, [entry], {postcode, index}, that.logger.writer);
                 }
-              }
 
-              if(JSON.parse(body).length -1 === bodyIndex && pcReqIsSuccess === false) {
+                if(JSON.parse(body).length -1 === bodyIndex && pcReqIsSuccess === false) {
 
-                var logData = that.logger.utils.dataMaker(null, 'pcReq200', response.statusCode, postcode.postcode + " " + postcode.city );
-                that.logger.writer(logData);
-                pcReqNoSuccess.push(pc[index]);
-              }
-            });
-          } else {
-            var logData = that.logger.utils.dataMaker(null, 'pcReqNot200', response.statusCode, postcode.postcode + " " + postcode.city );
-            that.logger.writer(logData);
-            pcReqNoSuccess.push(pc[index]);
-          }
+                  logData = that.logger.utils.dataMaker(null, 'pcReq200', response.statusCode, postcode, index);
+                  that.logger.writer(null, logData);                
+                }
+              });
+            } else {
+
+              logData = that.logger.utils.dataMaker(null, 'pcReqNot200', response.statusCode, postcode, index);
+              that.logger.writer(null, logData);
+            }
+          });
         });
       });
 
@@ -111,14 +122,15 @@ var jsonApiDePostcodes = function (api) {
 
     namesRange: function (req, res, next) {
 
-      var pcNamesZRangeMin = "[" + req.swagger.params.leadRegion.value;
-      var pcNamesZRangeMax = "[" + req.swagger.params.leadRegion.value + "\xff";
+      var pcNamesZRange = []
+      pcNamesZRange[0] = "[" + req.swagger.params.leadRegion.value;
+      pcNamesZRange[1] = "[" + req.swagger.params.leadRegion.value + "\xff";
 
-      that.db.names.range(pcNamesZKey, pcNamesZRangeMin, pcNamesZRangeMax, function(error, response) {
+      that.db.names.range(pcNamesZKey, pcNamesZRange, {}, function(error, response) {
 
         if (error) { return next(error); }
 
-        res.send(response);   
+        res.send(response);
       });
     },
 
@@ -126,7 +138,7 @@ var jsonApiDePostcodes = function (api) {
 
       var pcPositionsZMember = req.swagger.params.postcode.value;
 
-      that.db.positions.get(pcPositionsZKey, pcPositionsZMember, function (error, response) {
+      that.db.positions.get(pcPositionsZKey, [pcPositionsZMember], {}, function (error, response) {
         
         if (error) { return next(error); }
 
@@ -137,9 +149,9 @@ var jsonApiDePostcodes = function (api) {
 
     objectsGet: function (req, res, next) {
 
-      var pcObjectsHKey = req.swagger.params.de.value + ":" + req.swagger.params.postcodes.value + ":object:" + req.swagger.params.postcode.value;
+      var pcObjectsHKey = pcObjectsHPrefix + ":" + req.swagger.params.postcode.value;
 
-      that.db.objects.get(pcObjectsHKey, function (error, response) {
+      that.db.objects.get(pcObjectsHKey, [], {}, function (error, response) {
         
         if (error) { return next(error); }
 
@@ -150,6 +162,9 @@ var jsonApiDePostcodes = function (api) {
 };
 
 module.exports = jsonApiDePostcodes;
+
+
+                    
 
 
 
