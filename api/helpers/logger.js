e1e0e1a5d660047c4e0612abf0fb7f52eecd9393
arne-at-daten-und-bass <arne@daten-und-bass.io>
2017-11-16 11:04:37 +0100

    Added warn-file and improved logging accuracy

--- api/helpers/logger.js
+++ api/helpers/logger.js
@@ -10,6 +10,11 @@ var logger = new (winston.Logger)({
       level: 'info'
     }),
     new (winston.transports.File)({
+      name: 'warn-file',
+      filename: 'filelog-warn.log',
+      level: 'warn'
+    }),
+    new (winston.transports.File)({
       name: 'error-file',
       filename: 'filelog-error.log',
       level: 'error'
@@ -33,40 +38,66 @@ var logging = {
           case 'pcReq':
           case 'pcReq200':
           case 'pcReqNot200':
+          case 'pcReq200Empty':
             if (error) { 
               logging.postcodes.load.counter.error =  logging.postcodes.load.counter.error + 1; 
               data.counters = logging.postcodes.load.counter; 
-              logger.log('error', 'pcReq: Caught excesption', data); 
-            }
-            else { 
-              logging.postcodes.load.counter.error =  logging.postcodes.load.counter.error + 1;
-              data.counters = this.counter; 
-              logger.log('error', 'pcReq200 or pcReqNot200: Nothing found', data); 
-            } 
-          case 'namesAdd':
-          case 'positionsAdd':
-            if (data.response !== 1) { 
+              logger.log('error', data.action + ': Caught excesption ... no namesAdd executed', data);
+              logging.postcodes.load.counter.error =  logging.postcodes.load.counter.error + 1; 
+              data.counters = logging.postcodes.load.counter; 
+              logger.log('error', data.action + ': Caught excesption ... no positionsAdd executed', data);
+              logging.postcodes.load.counter.error =  logging.postcodes.load.counter.error + 1; 
+              data.counters = logging.postcodes.load.counter; 
+              logger.log('error', data.action + ': Caught excesption ... no objectsAdd executed', data);
+            } else { 
               logging.postcodes.load.counter.warn =  logging.postcodes.load.counter.warn + 1;
               data.counters = logging.postcodes.load.counter;
-              logger.log('warn', 'Wrong response', data); 
-            } 
-            if (data.response === 1) { 
-              logging.postcodes.load.counter.info =  logging.postcodes.load.counter.info + 1;
+              logger.log('warn', data.action + ': Nothing found ... no namesAdd executed', data);
+              logging.postcodes.load.counter.warn =  logging.postcodes.load.counter.warn + 1;
               data.counters = logging.postcodes.load.counter;
-              logger.log('info', 'Success', data); 
+              logger.log('warn', data.action + ': Nothing found ... no positionsAdd executed', data); 
+              logging.postcodes.load.counter.warn =  logging.postcodes.load.counter.warn + 1;
+              data.counters = logging.postcodes.load.counter;
+              logger.log('warn', data.action + ': Nothing found ... no objectsAdd executed', data); 
+            } 
+            break;
+
+          case 'namesAdd':
+          case 'positionsAdd':
+            if (error) { 
+              logging.postcodes.load.counter.error =  logging.postcodes.load.counter.error + 1;
+              data.counters = logging.postcodes.load.counter;
+              logger.log('error', data.action + ': Caught excesption', data);
+            } else {
+
+              if (data.response === 1) { 
+                logging.postcodes.load.counter.info =  logging.postcodes.load.counter.info + 1;
+                data.counters = logging.postcodes.load.counter;
+                logger.log('info', 'Success', data); 
+              } else {
+                logging.postcodes.load.counter.warn =  logging.postcodes.load.counter.warn + 1;
+                data.counters = logging.postcodes.load.counter;
+                logger.log('warn', 'Wrong response', data); 
+              }
             }
             break;
 
           case 'objectsAdd':
-            if (data.response !== 'OK') { 
-              logging.postcodes.load.counter.warn =  logging.postcodes.load.counter.warn + 1;
+            if (error) { 
+              logging.postcodes.load.counter.error =  logging.postcodes.load.counter.error + 1;
               data.counters = logging.postcodes.load.counter;
-              logger.log('warn', 'Wrong response', data); 
-            } 
-            if (data.response === 'OK') { 
-              logging.postcodes.load.counter.info =  logging.postcodes.load.counter.info + 1;
-              data.counters = logging.postcodes.load.counter;
-              logger.log('info', 'Success', data); 
+              logger.log('error', data.action + ': Caught excesption', data);
+            } else {
+
+              if (data.response === 'OK') { 
+                logging.postcodes.load.counter.info =  logging.postcodes.load.counter.info + 1;
+                data.counters = logging.postcodes.load.counter;
+                logger.log('info', 'Success', data); 
+              } else { 
+                logging.postcodes.load.counter.warn =  logging.postcodes.load.counter.warn + 1;
+                data.counters = logging.postcodes.load.counter;
+                logger.log('warn', 'Wrong response', data); 
+              } 
             }
             break;
 
@@ -79,19 +110,43 @@ var logging = {
             else { 
               logging.postcodes.load.counter.error =  logging.postcodes.load.counter.error + 1;
               data.counters = this.counter; 
-              logger.log('error', 'Unknown, uncaught excesption',data); 
+              logger.log('warn', 'Unknown, uncaught Event',data); 
             } 
         }
       },
+
+      reader: function (file, fields, line, callback) {
+        
+        var options = {
+          from: new Date(2000, 1, 1),
+          until: new Date(),
+          limit: 10,
+          start: 0,
+          order: 'desc',
+          fields: fields
+        };
+
+        logger.query(options, function (error, results) {
+          
+          if (error) {
+            return callback(error, null);
+          }
+          // console.log(results[file]);
+          if (results[file].length > 0) {
+
+            return callback(null, results[file][line].counters);
+          } else {
+
+            return callback(null, logging.postcodes.load.counter);
+          }
+          
+        });
+      },
       
       utils: {
-        dataMaker: function (error, action, response, member) {
-          return {
-            error: error,
-            action: action,
-            response: response,
-            member: member,
-          };
+        dataMaker: function (error, action, response, postcode, index) {
+
+          return {error, action, response, postcode, index};
         },
       },
     },
